d1a8649437f5e86ee493022ff004bd6860a86c3d
Salman Shah <sbs.191197@gmail.com>
2023-10-17 20:32:32 +0100

    Added slog to go starter code (#14)

--- pkg/handlers/post.go
+++ pkg/handlers/post.go
@@ -3,6 +3,17 @@ package handlers
 import "net/http"
 
 func post(w http.ResponseWriter, r *http.Request) {
+	// createdUserData, err := database.Create(db, tableName, helper.PopulateUserData())
+	// if err != nil {
+	// 	// TODO: Remove panic and add slog
+	// 	panic(err)
+	// }
+
+	// _, err = helper.UnmarshalUser(createdUserData)
+	// if err != nil {
+	// 	// TODO: Remove panic and add slog
+	// 	panic(err)
+	// }
 	w.WriteHeader(http.StatusBadRequest)
 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 }
